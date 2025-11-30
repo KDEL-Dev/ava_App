b7d1d39ddac4b7575b86998476926e9aac6811ff
kadelle liburd <lbrk0024@humber.ca>
2025-11-30 14:47:37 -0500

    add to github

---
+++ script.js
@@ -0,0 +1,57 @@
+// Medication Button onClick
+
+function changeMedsImg()
+{
+    document.getElementById('medsImg').src="img/icons/carbon_pills_onClick.png"
+}
+
+function returnMedsImg()
+{
+    document.getElementById('medsImg').src="img/icons/carbon_pills.png"
+}
+
+//Device Button onClick
+
+function changeDeviceImg()
+{
+    document.getElementById('deviceImg').src="img/icons/devices_onClick.png"
+}
+
+function returnDeviceImg()
+{
+    document.getElementById('deviceImg').src="img/icons/devices.png"
+}
+
+// Task Button onClick
+
+function changeTaskImg()
+{
+    document.getElementById('taskImg').src="img/icons/tasks_onClick.png"
+}
+
+function returnTaskImg()
+{
+    document.getElementById('taskImg').src="img/icons/tasks.png"
+}
+
+//Transport Button onClick
+
+function changeTransportImg()
+{
+    document.getElementById('transportImg').src="img/icons/transport_onClick.png"
+}
+
+function returnTransportImg()
+{
+    document.getElementById('transportImg').src="img/icons/transport.png"
+}
+
+//Read aloud Button onClick
+function changeReadImg()
+{
+    document.getElementById('readImg').src="img/icons/readAloud_onClick.png"
+}
+function returnReadImg()
+{
+    document.getElementById('readImg').src="img/icons/readAloud.png"
+}
